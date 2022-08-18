8d6912ed734170e87191e507ea9b2bec64714559
khelalfa rabah <khelalfarabeh1996@gmail.com>
2022-08-18 16:00:46 +0100

    Test, Build chronic prescription, chronic prescription item models

--- prescriptions/models.py
+++ prescriptions/models.py
@@ -1,3 +1,35 @@
+from time import time
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
+from django.db.models import CheckConstraint, Q, F
+import datetime
+
 
 # Create your models here.
+class ChronicPrescription(models.Model):
+    chronic_presecription_id = models.AutoField(primary_key=True)
+    date = models.DateField(default=datetime.date.today)
+    # This field is for the prescription duration in days
+    duration = models.PositiveIntegerField(default=90, validators=[MaxValueValidator(90)])
+    notification_status = models.BooleanField(default=True)
+
+    
+   
+    class Meta:
+        managed = True 
+        constraints = [
+            # Check for the date range, it should be between 90 days ago and today.
+            CheckConstraint(
+                check = Q(date__lte=datetime.date.today()) & 
+                Q(date__gte=datetime.date.today() - datetime.timedelta(days = 90)),
+                name="check_prescription_date_range"
+            ),
+                     
+        ]
+
+class PrescriptionItem(models.Model):
+    prescription_item_id = models.AutoField(primary_key=True)
+    drug_name = models.CharField(max_length=250, null=False, blank=False, unique=True)
+    quantity = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)])
+    prescription = models.ForeignKey('ChronicPrescription', on_delete=models.CASCADE, related_name="drugs")
---
+++ prescriptions/tests/test_models.py
@@ -0,0 +1,85 @@
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+import datetime 
+
+
+from prescriptions.models import ChronicPrescription, PrescriptionItem
+
+class PrescriptionItemModelTest(TestCase):
+
+    def test_item_related_to_prescription(self):
+        presc1 = ChronicPrescription.objects.create()
+        item1 = PrescriptionItem.objects.create(drug_name="Aprovel 300 mg b/30", quantity=5, prescription=presc1)
+        self.assertIn(item1, presc1.drugs.all())
+
+        presc2 = ChronicPrescription.objects.create()
+        item2 = PrescriptionItem.objects.create(drug_name="Novoformine 850 mg b/30", quantity=5, prescription=presc2)
+        self.assertIn(item2, presc2.drugs.all())
+
+        self.assertNotIn(item1, presc2.drugs.all())
+        self.assertNotIn(item2, presc1.drugs.all())
+    
+    def test_unique_item_by_name(self):
+        presc1 = ChronicPrescription.objects.create()
+        item1 = PrescriptionItem.objects.create(drug_name="Amarel 1 mg b/30", quantity=5, prescription=presc1)
+        item2 = PrescriptionItem(drug_name="Amarel 1 mg b/30", quantity=51, prescription=presc1)
+
+        with self.assertRaises(IntegrityError):
+            item2.save()
+            item2.full_clean()
+    
+    def testQuantityBiggerThanZero(self):
+        presc1 = ChronicPrescription.objects.create()
+        item1 = PrescriptionItem(drug_name="Amarel 1 mg b/30", quantity=0, prescription=presc1)
+
+        with self.assertRaises(ValidationError):
+            item1.save()
+            item1.full_clean()
+
+
+    
+
+    
+
+
+
+
+
+class ChronicPrescriptionModelTest(TestCase):
+    
+
+    def testDefaultDuration(self):
+        chronicPresc = ChronicPrescription.objects.create()
+        duration = chronicPresc.duration
+        self.assertEqual(duration, 90)
+    
+    def testMaxDuration(self):
+        chronicPresc = ChronicPrescription(duration=100)
+       # Should raise an error cause max duration is 90
+        with self.assertRaises(ValidationError):
+            chronicPresc.save()
+            chronicPresc.full_clean()
+    
+    def testDefaultNotificationStatus(self):
+        chronicPresc = ChronicPrescription.objects.create()
+        notification_status = chronicPresc.notification_status
+        self.assertTrue(notification_status)
+    
+
+    def testNoPrescriptionInFutur(self):
+        future_date = datetime.date(2025,2,24)
+        chronicPresc = ChronicPrescription(date=future_date)
+        
+        with self.assertRaises(IntegrityError):
+            chronicPresc.save()
+            chronicPresc.full_clean()
+    
+    def testNoOldPrescreptionDate(self):
+        old_date = datetime.date(1995,2,24)
+        chronicPresc = ChronicPrescription(date=old_date)
+        
+        with self.assertRaises(IntegrityError):
+            chronicPresc.save()
+            chronicPresc.full_clean()
+
