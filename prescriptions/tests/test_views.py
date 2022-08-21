0e5ba3b1734d111be9809be4eaae9e372fc76c87
khelalfa rabah <khelalfarabeh1996@gmail.com>
2022-08-21 13:04:39 +0100

    Test, build chronic prescription detail view

--- prescriptions/tests/test_views.py
+++ prescriptions/tests/test_views.py
@@ -6,7 +6,7 @@ from rest_framework.test import APITestCase
 import datetime
 
 from prescriptions.models import ChronicPrescription, PrescriptionItem
-from prescriptions.serializers import ChronicPrescriptionSerializer
+from prescriptions.serializers import ChronicPrescriptionSerializer, PrescriptionItemSerializer
 
 class ChronicPrescriptionListViewTestCase(APITestCase):
 
@@ -19,7 +19,7 @@ class ChronicPrescriptionListViewTestCase(APITestCase):
     
     def test_get_returns_json_200(self):
         response = self.client.get(self.url)
-        self.assertTrue(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response['content-type'], 'application/json')
     
     def test_return_prescription_list(self):
@@ -58,6 +58,60 @@ class ChronicPrescriptionListViewTestCase(APITestCase):
         response_data = json.loads(response.content.decode('utf8'))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
     
+class ChronicPrescrionDetailViewTestCase(APITestCase):
+
+    def setUp(self):
+        self.presc = ChronicPrescription.objects.create()
+        self.url = reverse('chronic_prescription_detail', kwargs={'pk': self.presc.chronic_prescription_id})
+        item_data = {'drug_name':"Dolipran cp 1g", 'quantity':1, 'prescription':self.presc}
+        self.item = PrescriptionItem.objects.create(**item_data)
+
+
+    def test_get_returns_json_200(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response['content-type'], 'application/json')
+    
+    def test_retrieve_prescription_detail(self):
+        response = self.client.get(self.url)
+        prescr_serializer = ChronicPrescriptionSerializer(instance=self.presc)
+        response_data = json.loads(response.content.decode('utf8'))
+        self.assertEqual(response_data, prescr_serializer.data)
+    
+    def test_retrieve_prescription_related_item(self):
+        # New prescription with its  new Item
+        new_presc = ChronicPrescription.objects.create()
+        data = {'drug_name':"Expandol cp 1g", 'quantity':11, 'prescription':new_presc}
+        new_item = PrescriptionItem.objects.create(**data)
+        new_item_serializer = PrescriptionItemSerializer(new_item)
+
+        old_item_serializer =  PrescriptionItemSerializer(self.item)
+        response = self.client.get(self.url)
+        response_data = json.loads(response.content.decode('utf8'))
+        prescr_serializer = ChronicPrescriptionSerializer(instance=self.presc)
+
+        self.assertIn(old_item_serializer.data, prescr_serializer.data['drugs'])
+        self.assertNotIn(new_item_serializer.data, prescr_serializer.data['drugs'])
+
+
+    def test_edit_prescription(self):
+        updated_data = {'chronic_prescription_id': self.presc.chronic_prescription_id, 'duration': 60}
+        old_notification_status = self.presc.notification_status
+        old_duration = self.presc.notification_status
+
+        response = self.client.put(self.url, updated_data)
+        response_data = json.loads(response.content.decode('utf8'))
+
+        self.assertEqual(response.status_code,  status.HTTP_200_OK)
+        self.assertEqual(response_data['notification_status'], old_notification_status)
+        self.assertNotEqual(response_data['duration'], old_duration)
+    
+    def test_delete_prescription(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(ChronicPrescription.objects.all().count(), 0)
+
+
 
 
 
--- prescriptions/urls.py
+++ prescriptions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ChronicPrescriptionListView
+from .views import ChronicPrescriptionListView, ChronicPrescriptionDetailView
 
 urlpatterns = [
     path('', ChronicPrescriptionListView.as_view(), name='chronic_prescription_list'),
+    path('<int:pk>/', ChronicPrescriptionDetailView.as_view(), name='chronic_prescription_detail'),
 
 ]
